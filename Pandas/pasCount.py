d50a83ff38cebeb4e142a64f84365a1fe5edaf2d
Sinegami <harrymgtu@gmail.com>
2019-05-13 20:32:17 +0300

    Update pasCount.py

--- Pandas/pasCount.py
+++ Pandas/pasCount.py
@@ -1,5 +1,5 @@
 import pandas # Import library
-outFile = open('output.txt', 'w', encoding='utf8')
-data = pandas.read_csv('titanic.csv', index_col='PassengerId')
-A = list(data['Sex'].value_counts())
+outFile = open('output.txt', 'w', encoding='utf8') # Open file for writing answer
+data = pandas.read_csv('titanic.csv', index_col='PassengerId') # Reading data from file
+A = list(data['Sex'].value_counts()) # Creating list with information about passengers
 print(A[0], A[1], end='', file=outFile)
