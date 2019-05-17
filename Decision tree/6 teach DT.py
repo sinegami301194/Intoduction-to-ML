55f67f0fbf1e35b0fcaba9ab089cb5d4a1fcb668
Sinegami <harrymgtu@gmail.com>
2019-05-18 00:59:27 +0300

    Add files via upload

---
+++ Decision tree/6 teach DT.py
@@ -0,0 +1,17 @@
+# Download a sample from the titanic.csv file using the Pandas package
+import pandas
+from sklearn.tree import DecisionTreeClassifier
+data = pandas.read_csv('titanic.csv', index_col='PassengerId')
+# Leave the sample four features
+x_labels = ['Pclass', 'Fare', 'Age', 'Sex']
+X = data.loc[:, x_labels]
+# Notice that the Sex attribute has string values
+X['Sex'] = X['Sex'].map(lambda sex: 1 if sex == 'male' else 0)
+# Highlight target variable
+y = data['Survived']
+# Find all objects that have missing features and remove them from the selection
+X = X.dropna()
+y = y[X.index.values]
+# Teach decision trees
+clf = DecisionTreeClassifier(random_state=241)
+clf.fit(X, y)
