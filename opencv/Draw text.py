12a51faff1177f087ec311b358f9b9587c287348
Sinegami <harrymgtu@gmail.com>
2019-05-20 23:51:40 +0300

    Add files via upload

---
+++ opencv/Draw text.py
@@ -0,0 +1,17 @@
+# Open image
+import cv2
+
+def viewImage(image, name_of_window):
+    cv2.namedWindow(name_of_window, cv2.WINDOW_NORMAL)
+    cv2.imshow(name_of_window, image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+image = cv2.imread('unnamed.jpg')
+output = image.copy()
+cv2.rectangle(output, (250, 100), (650, 600), (0, 255, 255), 10)
+cv2.line(output, (450, 100), (450, 600), (0, 0, 255), 5)
+cv2.putText(output, "Me using OpenCV", (10, 700),cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 255, 255), 4)
+viewImage(output, "Draw the rectangle")
+
+
