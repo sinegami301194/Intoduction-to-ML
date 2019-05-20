c1d60ffc8152ff9d3bd9aa4c749086156f88db7c
Sinegami <harrymgtu@gmail.com>
2019-05-20 23:44:03 +0300

    Add files via upload

---
+++ opencv/Draw rectangle.py
@@ -0,0 +1,13 @@
+# Open image
+import cv2
+
+def viewImage(image, name_of_window):
+    cv2.namedWindow(name_of_window, cv2.WINDOW_NORMAL)
+    cv2.imshow(name_of_window, image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+image = cv2.imread('unnamed.jpg')
+output = image.copy()
+cv2.rectangle(output, (250, 100), (650, 600), (0, 255, 255), 10)
+viewImage(output, "Draw the rectangle")
