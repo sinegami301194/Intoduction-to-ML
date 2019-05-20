9628b9d99053ab9357d5829f634b3226c4925fba
Sinegami <harrymgtu@gmail.com>
2019-05-20 23:53:54 +0300

    Update Draw line.py

--- opencv/Draw line.py
+++ opencv/Draw line.py
@@ -7,7 +7,7 @@ def viewImage(image, name_of_window):
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-image = cv2.imread('unnamed.jpg')
+image = cv2.imread('unnamed.jpg') # Read Image
 output = image.copy()
 cv2.rectangle(output, (250, 100), (650, 600), (0, 255, 255), 10)
 cv2.line(output, (450, 100), (450, 600), (0, 0, 255), 5)
