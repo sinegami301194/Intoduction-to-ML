4bbc638a17140356d6111847eb0aa70b1732182a
Sinegami <harrymgtu@gmail.com>
2019-05-27 23:49:03 +0300

    Update UItest.py

--- Python GUI/Test GUI/UItest.py
+++ Python GUI/Test GUI/UItest.py
@@ -2,7 +2,7 @@ from PyQt5 import QtWidgets, uic # Include Libraries
 
 
 app = QtWidgets.QApplication([])
-dlg = uic.loadUi('test1.ui')
+dlg = uic.loadUi('test1.ui') # Load UI from file
 
 dlg.show()
 app.exec()
