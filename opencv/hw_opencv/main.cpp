b2351d7661f9f45d746415fe4bcd9499df812e4a
Sinegami <harrymgtu@gmail.com>
2019-06-21 18:03:47 +0300

    Update main.cpp

--- opencv/hw_opencv/main.cpp
+++ opencv/hw_opencv/main.cpp
@@ -15,17 +15,27 @@ using namespace cv;
 int main(int argc, char* argv[])
 {
     QCoreApplication a(argc, argv);
-    string Image_PATH = "/home/alpatikov_i/Pictures/p2.jpg";
-    Mat img = imread(Image_PATH, IMREAD_COLOR);
 
-    rectangle(img,Rect(210,80,50,40), Scalar(255,128,128),2);
-    putText(img, "Differencing the two images.", cvPoint(30,30),
-            FONT_HERSHEY_COMPLEX_SMALL, 1.0, cvScalar(255,1,128), 1, CV_AA);
-    
+    string Image_PATH = "/home/alpatikov_i/Pictures/p2.jpg";
+    string Text_info;
+    const String& Video_PATH = "/testdata/tor/inp/tor.028/tor.028.021.left.avi";
+
+    //Mat img = imread(Image_PATH, IMREAD_COLOR);
+    Mat frame;
+    VideoCapture cap(Video_PATH);
+    cap >> frame;
+    putText(frame, "Frame number: 0", cvPoint(30,30),
+           FONT_HERSHEY_COMPLEX_SMALL, 1.0, cvScalar(128,255,255), 1, CV_AA);
+    //rectangle(img,Rect(394, 114, 92, 76), Scalar(255,128,128),2);
+
+    rectangle(frame,Rect(394, 114, 92, 76), Scalar(128,255,255),2);
+
     namedWindow("win",WINDOW_AUTOSIZE);
-    imshow("win",img);
+    //imshow("win",img);
+    imshow("win",frame);
+    imwrite("/home/alpatikov_i/Pictures/p5.jpg", frame);
     waitKey(0);
-    return 1;
+    destroyAllWindows();
 
     return 0;
 }
