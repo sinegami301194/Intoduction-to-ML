d73d81a62587be07711a7329734e337f7cc89a1a
Sinegami <harrymgtu@gmail.com>
2020-01-31 11:15:00 +0300

    Update Fourier_transform.py

--- opencv/small_tasks/Fourier_transform.py
+++ opencv/small_tasks/Fourier_transform.py
@@ -1,1 +1,29 @@
 import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+img = cv2.imread('/home/alpatikov_i/Pictures/Screenshot from rzd.013.005.left.avi.png', 0)
+#f = np.fft.fft2(img)
+#fshift = np.fft.fftshift(f)
+#magnitude_spectrum = 20*np.log(np.abs(fshift))
+
+dst = cv2.GaussianBlur(img,(5,5),cv2.BORDER_DEFAULT) 
+dft = cv2.dft(np.float32(dst),flags = cv2.DFT_COMPLEX_OUTPUT)
+dft_shift = np.fft.fftshift(dft)
+
+magnitude_spectrum = 20*np.log(cv2.magnitude(dft_shift[:,:,0],dft_shift[:,:,1]))
+
+plt.subplot(121),plt.imshow(dst, cmap = 'gray')
+plt.title('Input Image'), plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(magnitude_spectrum, cmap = 'gray')
+plt.title('Magnitude Spectrum'), plt.xticks([]), plt.yticks([])
+plt.show()
+
+
+#plt.subplot(121),plt.imshow(img, cmap = 'gray')
+#plt.title('Input Image'), plt.xticks([]), plt.yticks([])
+#plt.subplot(122),plt.imshow(magnitude_spectrum, cmap = 'gray')
+#plt.title('Magnitude Spectrum'), plt.xticks([]), plt.yticks([])
+#plt.show()
+#cv2.imshow("first", img)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
